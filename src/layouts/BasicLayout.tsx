48533f42116ffa5480721cac07b84592f7c957ce
renyel <reniel.salvador@gmail.com>
2023-09-30 00:12:08 +0800

    clean

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,5 +1,5 @@
 import { Link, Outlet } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { cn } from '../shared/utils';
 
 interface ToggleButtonProps {
